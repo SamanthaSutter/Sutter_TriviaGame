fa9125bb8967f1eb97aa1b36d38eae1009f5cb14
Student <Student@IT-DESKTOP>
2023-04-23 13:52:05 -0500

    Add project files.

---
+++ Sutter_TriviaGame/Info.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Sutter_TriviaGame
+{
+    class Info
+    {
+        public void DisplayInfo(string assignment)
+        {
+            const string barrier = "***********************************************" +
+                "********************";
+            WriteLine("\n\n\n" + barrier);
+            WriteLine("Name:\t\t Samantha Sutter");
+            WriteLine("Instructor:\t Janese Christie");
+            WriteLine("Assignment:\t " + assignment);
+            WriteLine("Date:\t\t " + DateTime.Today.ToShortDateString());
+            WriteLine(barrier);
+            WriteLine("\n\nType any key to continue");
+            ReadKey();
+            Clear();
+        }
+    }
+}
+
---
+++ Sutter_TriviaGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sutter_TriviaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info info = new Info();
+            Controller cntrl = new Controller();
+
+            info.DisplayInfo("Assignment 10 - Trivia Game");
+            cntrl.Play();
+        }
+    }
+}
